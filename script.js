a28c81867d568e0ea2a51b289f2f4bf573298a1e
aryansighkumaya <98814995+aryansighkumaya@users.noreply.github.com>
2024-12-21 00:40:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
-// Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
+  this.make = make; // Assign make to the instance
+  this.model = model; // Assign model to the instance
+}
 
-function SportsCar(make, model, topSpeed) {}
+// Add the getMakeModel method to the Car prototype
+Car.prototype.getMakeModel = function () {
+  return `${this.make} ${this.model}`; // Return make and model as a string
+};
+
+function SportsCar(make, model, topSpeed) {
+  // Call the Car constructor to initialize make and model
+  Car.call(this, make, model);
+  this.topSpeed = topSpeed; // Assign topSpeed to the instance
+}
+
+// Inherit Car's prototype in SportsCar
+SportsCar.prototype = Object.create(Car.prototype);
+
+// Reset the constructor to SportsCar
+SportsCar.prototype.constructor = SportsCar;
+
+// Add the getTopSpeed method to the SportsCar prototype
+SportsCar.prototype.getTopSpeed = function () {
+  return this.topSpeed; // Return the top speed
+};
 
 // Do not change the code below
 window.Car = Car;
